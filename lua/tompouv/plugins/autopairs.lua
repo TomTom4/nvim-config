921cd4804dfea1454252d615a010f85d7360778d
TomPouv <contact@thomas-pouvreau.fr>
2025-03-28 10:43:45 +0100

    chore: rename and fix autopairs bug: failed config

--- lua/tompouv/plugins/autopairs.lua
+++ lua/tompouv/plugins/autopairs.lua
@@ -1,14 +1,15 @@
 return {
   "windwp/nvim-autopairs", 
-  event = {"InsertEnter"},
+  event = "InsertEnter",
   dependencies = {
     "hrsh7th/nvim-cmp",
   },
   config = function()
     -- import nvim-autopairs
     local autopairs = require('nvim-autopairs')
+
     autopairs.setup({
-      check_ts = true,
+      check_ts = true, --enable treesitter
       ts_config = {
         lua = {"string"},
         javascript = { "template_string" },
@@ -22,6 +23,6 @@ return {
     local cmp = require("cmp")
 
     -- make autopairs and completion work together
-    cmp.event.on("confirm_done", cmp_autopairs.on_confirm_done())
-  end
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
 }
