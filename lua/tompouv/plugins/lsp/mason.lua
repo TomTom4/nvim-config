bf4163cab2dc84d9e25f8b71f82f19e8a7c94e42
TomPouv <contact@thomas-pouvreau.fr>
2025-03-28 10:56:06 +0100

    chore: basic configuration for mason with lsp servers

--- lua/tompouv/plugins/lsp/mason.lua
+++ lua/tompouv/plugins/lsp/mason.lua
@@ -1,3 +1,36 @@
 return{
   "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    -- "neovim/nvim-lspconfig",
+  },
+  config = function()
+    local mason = require("mason")
+    mason.setup({
+      ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗"
+            }
+      }
+    })
+
+    local masonLspconfig = require("mason-lspconfig")
+    masonLspconfig.setup(
+      {
+        ensure_installed = {
+          "pylsp", -- python lsp server
+          "dockerls", -- docker lsp server
+          "ts_ls", -- typescript lsp server
+          "tailwindcss", -- tailwindcss lsp server
+          "lua_ls", -- lua lsp server
+          "ast_grep", -- c, c++, c# lsp server
+          "rust_analyzer", -- rust lsp server
+          "sqlls", -- sql lsp server
+
+        },
+      }
+    )
+  end
 }
