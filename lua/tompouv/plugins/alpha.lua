3286fa6454f2583dd24d0a30bb1c2d7e1380209e
TomPouv <contact@thomas-pouvreau.fr>
2025-03-28 14:49:11 +0100

    chore: add which key on the greeter panel

--- lua/tompouv/plugins/alpha.lua
+++ lua/tompouv/plugins/alpha.lua
@@ -1,42 +1,41 @@
 return {
-  'goolord/alpha-nvim',
-  event = 'VimEnter',
-  config = function()
-    local alpha = require('alpha')
-    local dashboard = require('alpha.themes.dashboard')
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    dashboard.section.header.val = {
-        "                                                    ",
-        " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-        " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-        " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-        " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-        " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-        " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    }
+		dashboard.section.header.val = {
+			"                                                    ",
+			" ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			" ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			" ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			" ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			" ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			" ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+		}
 
-    -- Set menu
-    dashboard.section.buttons.val = {
-        dashboard.button( "e", "  > New file" , "<cmd>ene<CR>"),
-        dashboard.button( "SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-        dashboard.button( "SPC ff", "  > Find a file"   , "<cmd>Telescope find_files<CR>"),
-        dashboard.button( "SPC wr", "󱄋  > restore last session" , "<cmd>SessionRestore<CR>"),
-        dashboard.button( "q", "󰩈  > Quit NVIM", ":qa<CR>"),
-    }
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  > New file", "<cmd>ene<CR>"),
+			dashboard.button("SPC", "?  > which key", "<cmd>WhichKey<CR>"),
+			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("SPC ff", "  > Find a file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("SPC wr", "󱄋  > restore last session", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "󰩈  > Quit NVIM", ":qa<CR>"),
+		}
 
-    -- send config to alphaalpha.setup(dashboard.opts)
+		-- send config to alphaalpha.setup(dashboard.opts)
 
--- Disable folding on alpha buffer
-vim.cmd([[
+		-- Disable folding on alpha buffer
+		vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
 ]])
-    alpha.setup(dashboard.opts)
+		alpha.setup(dashboard.opts)
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[
+		-- Disable folding on alpha buffer
+		vim.cmd([[
         autocmd FileType alpha setlocal nofoldenable
     ]])
-    
-
-  end,
+	end,
 }
