8ced1f257ce7a5edefdd609649c1d0511db2d3d5
TomPouv <contact@thomas-pouvreau.fr>
2025-03-28 14:20:42 +0100

    chore: add linting

--- lua/tompouv/plugins/lsp/mason.lua
+++ lua/tompouv/plugins/lsp/mason.lua
@@ -1,47 +1,46 @@
-return{
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "whoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"whoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
 
-    mason.setup({
-      ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗"
-            }
-      }
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    local masonLspconfig = require("mason-lspconfig")
+		local masonLspconfig = require("mason-lspconfig")
 
-    masonLspconfig.setup(
-      {
-        ensure_installed = {
-          "pylsp", -- python lsp server
-          "dockerls", -- docker lsp server
-          "ts_ls", -- typescript lsp server
-          "tailwindcss", -- tailwindcss lsp server
-          "lua_ls", -- lua lsp server
-          "ast_grep", -- c, c++, c# lsp server
-          "rust_analyzer", -- rust lsp server
-          "sqlls", -- sql lsp server
-        },
-      }
-    )
+		masonLspconfig.setup({
+			ensure_installed = {
+				"pylsp", -- python lsp server
+				"dockerls", -- docker lsp server
+				"ts_ls", -- typescript lsp server
+				"tailwindcss", -- tailwindcss lsp server
+				"lua_ls", -- lua lsp server
+				"ast_grep", -- c, c++, c# lsp server
+				"rust_analyzer", -- rust lsp server
+				"sqlls", -- sql lsp server
+			},
+		})
 
-    local mason_tool_installer = require('mason-tool-installer')
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "stylua",
-        "isort",
-        "ruff",
-      }
-    })
-  end
+		local mason_tool_installer = require("mason-tool-installer")
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"ruff",
+				"eslint_d",
+			},
+		})
+	end,
 }
