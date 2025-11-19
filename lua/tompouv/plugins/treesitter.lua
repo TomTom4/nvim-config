b1de9bf890f61c7a0dc02a52137fde98a729fa15
Thomas Pouvreau <thomaspouvreau@macbook-air-de-thomas.home>
2025-11-19 12:07:04 +0100

    migration: go to the new treesitter implementation

--- lua/tompouv/plugins/treesitter.lua
+++ lua/tompouv/plugins/treesitter.lua
@@ -1,40 +1,44 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	event = { "BufReadPre", "BufNewFile" },
+	branch = "main",
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
 	},
 
 	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+		local treesitter = require("nvim-treesitter")
 
+		local ensure_installed = {
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"lua",
+			"python",
+			"bash",
+			"c",
+			"dockerfile",
+			"html",
+			"css",
+			"gitignore",
+			"vim",
+			"vimdoc",
+			"yaml",
+			"sql",
+			"ruby",
+			"rust",
+			"markdown",
+			"markdown_inline",
+		}
+
+		treesitter.install(ensure_installed)
 		-- configure treesitter
 		treesitter.setup({
 			highlight = { enable = true },
 			indent = { enable = true },
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"lua",
-				"python",
-				"bash",
-				"c",
-				"dockerfile",
-				"html",
-				"css",
-				"gitignore",
-				"vim",
-				"vimdoc",
-				"yaml",
-				"sql",
-				"ruby",
-				"rust",
-				"markdown",
-				"markdown_inline",
-			},
 			incremental_selection = {
 				enable = true,
 				keymaps = {
